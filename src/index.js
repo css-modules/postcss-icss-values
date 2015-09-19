3d4725ebdae4802ed0bac5b987c4fe207a5360d4
Glen Maddern <glenmaddern@gmail.com>
2015-09-19 15:14:36 +0200

    fleshing out the tests a bit further

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ export default css => {
         }
       })
       importAliases.push({path, imports})
-      atRule.removeSelf()
+      atRule.remove()
     }
   })
 
--- test/index.js
+++ test/index.js
@@ -12,11 +12,52 @@ const test = (input, expected) => {
 
 describe('constants', () => {
   it('should pass through an empty string', () => {
-    test('','')
+    test('', '')
   })
 
   it('should export a constant', () => {
-    test('@define red blue;',':export {\n  red: blue\n}')
+    test('@define red blue;', ':export {\n  red: blue\n}')
+  })
+
+  it('should export a more complex constant', () => {
+    test('@define small (max-width: 599px);', ':export {\n  small: (max-width: 599px)\n}')
+  })
+
+  it('should replace constants within the file', () => {
+    test('@define blue red; .foo { color: blue; }', ':export {\n  blue: red;\n}\n.foo { color: red; }')
+  })
+
+  it('should import a simple constant', () => {
+    test('@import red from "./colors.css";', ':import("./colors.css") {\n  i__const_red_0: red\n}')
+  })
+
+  it('should import a simple constant and replace usages', () => {
+    test('@import red from "./colors.css"; .foo { color: red; }', ':import("./colors.css") {\n  i__const_red_1: red;\n}\n.foo { color: i__const_red_1; }')
+  })
+
+  it('should import and alias a constant and replace usages', () => {
+    test('@import blue as red from "./colors.css"; .foo { color: red; }', ':import("./colors.css") {\n  i__const_red_2: blue;\n}\n.foo { color: i__const_red_2; }')
+  })
+
+  it('should import multiple from a single file', () => {
+    test(
+      `@import blue, red from "./colors.css";
+.foo { color: red; }
+.bar { color: blue }`,
+      `:import("./colors.css") {
+  i__const_blue_3: blue;
+  i__const_red_4: red;
+}
+.foo { color: i__const_red_4; }
+.bar { color: i__const_blue_3 }`)
+  })
+
+  it('should import from a definition', () => {
+    test(
+      '@define colors: "./colors.css"; @import red from colors;',
+      ':export {\n  colors: "./colors.css"\n}\n' +
+      ':import("./colors.css") {\n  i__const_red_5: red\n}'
+    )
   })
 })
 
